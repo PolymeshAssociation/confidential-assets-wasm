ae900a906273d0da14b24954948586e1bb21ed65
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2025-11-21 01:25:06 +0800

    FIXUP: build with client script.

--- build_with_rust_client.sh
+++ build_with_rust_client.sh
@@ -2,13 +2,13 @@
 set -e
 
 echo "Building WASM package with Rust client for web..."
-wasm-pack build --release --features polymesh-client --target web --out-dir pkg-web --scope polymesh
+wasm-pack build --release --target web --out-dir pkg-web --scope polymesh --features polymesh-client
 
 echo "Building WASM package with Rust client for Node.js..."
-wasm-pack build --release --features polymesh-client --target nodejs --out-dir pkg-node --scope polymesh
+wasm-pack build --release --target nodejs --out-dir pkg-node --scope polymesh --features polymesh-client
 
 echo "Building WASM package with Rust client for bundlers..."
-wasm-pack build --release --features polymesh-client --target bundler --out-dir pkg --scope polymesh
+wasm-pack build --release --target bundler --out-dir pkg --scope polymesh --features polymesh-client
 
 echo "Build complete!"
 echo "- Web package: pkg-web/"
